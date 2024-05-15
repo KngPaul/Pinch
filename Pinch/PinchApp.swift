3c9cfd407ecfab621eb7901a3c05e00713fd63f4
Paul Onawola <onawola.ire@gmail.com>
2024-05-15 13:21:35 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Paul Onawola on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
